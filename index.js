710561a2203402127dcf3b344417ef54b74236cb
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-08-07 14:18:37 +0200

    fix parent include (correct statement)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   name: '@lblod/ember-rdfa-editor-standard-template-plugin',
 
   included: function (app) {
-    this._super.included(app);
+    this._super.included.apply(this, app);
     app.import('vendor/node-uuid-v4_v3.1.0.js');
     app.import('vendor/shims/uuid.js');
   }
