27ca18d034faefd59b8511be11a95eb8885f5c78
Niels Vandekeybus <progster@gmail.com>
2021-09-02 12:15:09 +0200

    linting

--- tests/unit/utils/instantiate-uuids-test.js
+++ tests/unit/utils/instantiate-uuids-test.js
@@ -4,8 +4,9 @@ import { module, test } from 'qunit';
 module('Unit | Utility | instantiate-uuids', function () {
   // Replace this with your real tests.
   test('it works', function (assert) {
-    const templateString = 'http://data.lblod.info/id/besluiten/${generateUuid()}"';
+    const templateString =
+      'http://data.lblod.info/id/besluiten/${generateUuid()}"';
     let result = instantiateUuids(templateString);
-    assert.ok(! result.includes('generateUuid'));
+    assert.ok(!result.includes('generateUuid'));
   });
 });
