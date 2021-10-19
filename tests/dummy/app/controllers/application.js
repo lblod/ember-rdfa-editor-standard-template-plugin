174701aa4a98264d7d9e2ead8b72050827470cc1
abeforgit <arnebertrand@gmail.com>
2021-10-20 00:54:07 +0200

    Add proper dummy test tooling

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,17 +1,164 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import xmlFormat from 'xml-formatter';
+import { basicSetup, EditorState, EditorView } from '@codemirror/basic-setup';
+import { xml } from '@codemirror/lang-xml';
+import { html } from '@codemirror/lang-html';
 
 export default class ApplicationController extends Controller {
+  @tracked debug;
+  @tracked xmlDebuggerOpen = false;
+  @tracked debuggerContent = '';
+  @tracked htmlDebuggerOpen = false;
+  @tracked _editorController;
+  unloadListener;
+  xmlEditor;
+  htmlEditor;
   plugins = ['standard-template'];
   controller;
 
+
+  get editorController() {
+    if (!this._editorController) {
+      throw new Error('Accessing controller before editor init');
+    }
+    return this._editorController;
+  }
+
+  get formattedXmlContent() {
+    if (this.debuggerContent) {
+      try {
+        return xmlFormat(this.debuggerContent);
+      } catch (e) {
+        return this.debuggerContent;
+      }
+    }
+    return this.debuggerContent;
+  }
+
+  setup() {
+    this.unloadListener = () => {
+      this.saveEditorContentToLocalStorage();
+    };
+    window.addEventListener('beforeunload', this.unloadListener);
+  }
+
+  teardown() {
+    if (this.unloadListener) {
+      window.removeEventListener('beforeunload', this.unloadListener);
+    }
+  }
+
+  @action
+  initDebug(info) {
+    this.debug = info;
+  }
+
+  @action
+  setupXmlEditor(element) {
+    this.xmlEditor = new EditorView({
+      state: EditorState.create({
+        extensions: [basicSetup, xml()],
+      }),
+      parent: element,
+    });
+    this.xmlEditor.dispatch({
+      changes: { from: 0, insert: this.debuggerContent },
+    });
+  }
+
+  @action
+  setupHtmlEditor(element) {
+    this.htmlEditor = new EditorView({
+      state: EditorState.create({
+        extensions: [basicSetup, html()],
+      }),
+      parent: element,
+    });
+    this.htmlEditor.dispatch({
+      changes: { from: 0, insert: this.debuggerContent },
+    });
+  }
+
+  setHtmlContent(content) {
+    this.editorController.executeCommand(
+      'insert-html',
+      content,
+      this.editorController.rangeFactory.fromAroundAll()
+    );
+  }
+
+  setXmlContent(content) {
+    this.editorController.executeCommand(
+      'insert-xml',
+      content,
+      this.editorController.rangeFactory.fromAroundAll()
+    );
+  }
+
   @action
   rdfaEditorInit(controller) {
-    this.controller = controller;
-    controller.executeCommand(
-      'insert-html',
-      `<div prefix="besluit: http://data.vlaanderen.be/ns/besluit#" typeof="besluit:Besluit"><div property="heeftBehandeling" typeof="besluit:BehandelingVanAgendapunt">skeet</div>yeet</div>`,
-      controller.rangeFactory.fromAroundAll()
-    );
+    const presetContent = localStorage.getItem('EDITOR_CONTENT') ?? '';
+    this._editorController = controller;
+    console.log(this.editorController);
+    this.setHtmlContent(presetContent);
+    const editorDone = new CustomEvent('editor-done');
+    window.dispatchEvent(editorDone);
+  }
+
+  @action
+  setDebuggerContent(content) {
+    this.debuggerContent = content;
+  }
+
+  @action
+  setEditorContent(type, content) {
+    if (this._editorController) {
+      if (type === 'html') {
+        this.setHtmlContent(content);
+        this.saveEditorContentToLocalStorage();
+      } else {
+        this.setXmlContent(content);
+        this.saveEditorContentToLocalStorage();
+      }
+    }
+  }
+
+  @action openContentDebugger(type) {
+    if (this._editorController) {
+      if (type === 'xml') {
+        // this.debuggerContent = this.rdfaEditor.xmlContentPrettified;
+        this.xmlDebuggerOpen = true;
+      } else {
+        // this.debuggerContent = this.rdfaEditor.htmlContent;
+        this.htmlDebuggerOpen = true;
+      }
+    }
+  }
+
+  @action closeContentDebugger(type, save) {
+    if (type === 'xml') {
+      this.debuggerContent = this.xmlEditor.state.sliceDoc();
+      this.xmlDebuggerOpen = false;
+    } else {
+      this.debuggerContent = this.htmlEditor.state.sliceDoc();
+      this.htmlDebuggerOpen = false;
+    }
+    if (save) {
+      const content = this.debuggerContent;
+      if (!content) {
+        //xml parser doesn't accept an empty string
+        this.setEditorContent('html', '');
+      } else {
+        this.setEditorContent(type, content);
+      }
+    }
+  }
+
+  saveEditorContentToLocalStorage() {
+    if (this._editorController) {
+      // localStorage.setItem("EDITOR_CONTENT", this.rdfaEditor.htmlContent);
+    }
   }
 }
