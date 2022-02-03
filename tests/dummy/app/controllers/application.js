02c784eb603f250655ac536f0bb5d444d5080484
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-02-04 00:06:40 +0100

    Put the correct context in the editor
    Putting the correct RDFa context in the editor made it work. It was
    missing the `besluit:BeslissingVanAgendapunt` to make the plugin
    trigger. This took hours of debugging and could have been explained in a
    README to avoid having to go through this.

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -6,7 +6,11 @@ export default class ApplicationController extends Controller {
 
   @action
   rdfaEditorInit(controller) {
-    const presetContent = localStorage.getItem('EDITOR_CONTENT') ?? '';
+    //const presetContent = localStorage.getItem('EDITOR_CONTENT') ?? '';
+    const presetContent = `
+<div prefix="ext: http://mu.semte.ch/vocabularies/ext/ mobiliteit: https://data.vlaanderen.be/ns/mobiliteit# dct: http://purl.org/dc/terms/" typeof="besluit:BehandelingVanAgendapunt" resource="http://data.lblod.info/artikels/32f2768c-917f-412a-a33e-45b2722eb610">
+    Enter this text with the cursor and try to add a template. This wil appear in place of the cursor leaving this text on screen, but when it works, it works. We could consider adding the RDFa data of this element into the debug component itself.
+</div>`;
     controller.setHtmlContent(presetContent);
     const editorDone = new CustomEvent('editor-done');
     window.dispatchEvent(editorDone);
