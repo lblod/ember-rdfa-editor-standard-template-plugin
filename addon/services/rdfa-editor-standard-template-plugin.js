a060ee1be1ccb709af7532439ab1d9f9f750b86c
Niels Vandekeybus <progster@gmail.com>
2022-03-03 12:31:26 +0100

    restore functionality used by GN

--- addon/services/rdfa-editor-standard-template-plugin.js
+++ addon/services/rdfa-editor-standard-template-plugin.js
@@ -1,5 +1,6 @@
 import Service, { inject as service } from '@ember/service';
 import { task, waitForProperty } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
 
 export default class RdfaEditorStandardTemplatePluginService extends Service {
   @service store;
@@ -20,5 +21,25 @@ export default class RdfaEditorStandardTemplatePluginService extends Service {
     this.templates = await this.store.query('template', {
       fields: { templates: 'title,contexts,matches,disabled-in-contexts' },
     });
+  }
+
+  /**
+     Filter the valid templates for a context.
+     @method templatesForContext
+     @param {Array} Array of templates
+     @param {Array} The path of rdfaContext objects from the root till the current context
+     @return {Array} Array of templates (filtered)
+     @private
+  */
+  templatesForContext(templates, rdfaTypes) {
+    let isMatchingForContext = (template) => {
+      return (
+        rdfaTypes.filter((e) => template.get('contexts').includes(e)).length >
+          0 &&
+          rdfaTypes.filter((e) => template.get('disabledInContexts').includes(e))
+          .length === 0
+      );
+    };
+    return templates.filter(isMatchingForContext);
   }
 }
